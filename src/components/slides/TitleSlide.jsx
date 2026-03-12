90275d7017028e0b4539eaa0937d361f4caf7dc0
Manikandan <shanmugam.manikandan@cognizant.com>
2026-03-12 21:39:11 +0800

    disable Scroll indicator

--- src/components/slides/TitleSlide.jsx
+++ src/components/slides/TitleSlide.jsx
@@ -53,11 +53,11 @@ export default function TitleSlide() {
             </div>
 
             {/* Scroll indicator */}
-            <div className="absolute bottom-6 md:bottom-12 left-1/2 -translate-x-1/2 animate-bounce">
+            {/* <div className="absolute bottom-6 md:bottom-12 left-1/2 -translate-x-1/2 animate-bounce">
                 <svg className="w-6 h-6 md:w-8 md:h-8 text-white/40" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
                 </svg>
-            </div>
+            </div> */}
         </div>
     )
 }
